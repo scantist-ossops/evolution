fa18412d8dbc3801659185b08dbcb65fbefd6bed
Yamamoto <yamamoto@kyms.jp>
2022-12-04 11:28:47 +0900

    improved - DBAPI->_getFromStringFromArray()

--- manager/includes/extenders/dbapi.mysqli.class.inc.php
+++ manager/includes/extenders/dbapi.mysqli.class.inc.php
@@ -731,7 +731,7 @@ class DBAPI
     {
         $_ = array();
         foreach ($tables as $k => $v) {
-            $_[] = $v;
+            $_[] = is_int($k) ? $v : $v . $v.' '.$k;
         }
 
         return implode(' ', $_);
