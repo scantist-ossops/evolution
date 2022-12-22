e1d3702ac853e9ba793021ffa06fef236cf37031
BBloke <bbloke@blueyonder.co.uk>
2022-12-22 15:31:20 +0000

    Update document.parser.class.inc.php # Conflicts: #	manager/includes/document.parser.class.inc.php

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -4406,7 +4406,7 @@ class DocumentParser
                     if ($this->config['aliaslistingfolder'] == 1  || (isset($this->config['full_aliaslisting']) && $this->config['full_aliaslisting'] == 1)) {
                         $al = $this->getAliasListing($id);
                     } else {
-                        $al = empty($aliasListing[$id]) ? $this->aliasListing[$this->config['error_page']] : $this->aliasListing[$id];
+                        $al = $this->aliasListing[$id];
                     }
                     if (!empty($al)) {
                         if ($al['isfolder'] === 1 && $this->config['make_folders'] === '1') {
